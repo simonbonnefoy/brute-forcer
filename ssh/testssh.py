bb99c692ee35e72fc2581b92089132ee74ecba43
Simon Bonnefoy <bonnefoy.simon@protonmail.com>
2019-03-26 13:04:59 +0100

    test

--- ssh/testssh.py
+++ ssh/testssh.py
@@ -5,6 +5,6 @@ import paramiko
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 try :
-    ssh.connect('warp.ifh.de', username='bonnefoy', password='test')
+    ssh.connect(host username='user', password='test')
 except paramiko.ssh_exception.AuthenticationException:
     print("authentication failed")
