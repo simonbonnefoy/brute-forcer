50d4474439cdf8d91e06fe68d333d083aab32ae7
Simon Bonnefoy <bonnefoy.simon@protonmail.com>
2019-04-01 12:44:27 +0000

    Update testssh.py

--- ssh/testssh.py
+++ ssh/testssh.py
@@ -1,10 +1,29 @@
-#!/usr/bin/env python
-
+import multiprocessing as mp
 import paramiko
+import time
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-try :
-    ssh.connect(host username='user', password='test')
-except paramiko.ssh_exception.AuthenticationException:
-    print("authentication failed")
+def ssh(password):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try :
+        print('Testing password: ', password)
+        ssh.connect('127.0.0.1', username='user', password=password)
+        return True
+    except paramiko.ssh_exception.AuthenticationException:
+        print("authentication failed")
+        return False
+
+
+if __name__ == '__main__':
+    pass_list = ['test1','test2','test3', 'test3', 'test3', 'test3', 'test3']
+    pool = mp.Pool(processes=2)
+    t1 = time.time()
+    '''for synchronous'''
+    [pool.apply(ssh, args=(x,)) for x in pass_list]
+    '''For Asynchronous'''
+    results = [pool.apply_async(ssh, args=(x,)) for x in pass_list]
+    pool = mp.Pool(processes=4)
+    output = [p.get() for p in results]
+    print(output)
+    print('Code executed in: ', time.time() - t1)
